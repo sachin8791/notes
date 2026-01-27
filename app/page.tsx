ead62b8d6f843254a2bb0e431499f55540eb9e04
sachin8791 <sachinkumar879162@gmail.com>
2026-01-28 00:39:43 +0530

    feat: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
     return (
     <div className="text-yellow-500">
       Hello
+      <Button>click me</Button>
     </div>
     );
   }
